09d08ba9a0682ec01527d3220627f4c3b916dbe0
marcosandremoura <marcosandremoura@gmail.com>
2024-03-23 19:19:23 -0300

    half of the module

---
+++ tipos-de-dados/4_string/script.js
@@ -0,0 +1,7 @@
+
+
+console.log(typeof "Oi, meu nome é Marcos");
+console.log(typeof 'Eu queria conseguir um trabalho na área da programação.');
+console.log(typeof `Teste`);
+console.log(typeof "Infinity");
+console.log(typeof Infinity);
---
+++ tipos-de-dados/5_string-avancado/script.js
@@ -0,0 +1,14 @@
+
+
+// ( "\n" PULA A LINHA )
+console.log("Primeira linha \nSegunda linha");
+
+// ( PARA INSERIR UMA PALAVRA COM ASPAS, DEVE COLOCAR DIFERENTE DA USADA NA STRING )
+console.log("O meu nome é 'Marcos'");
+console.log('O nome dela é "Cristiane"');
+
+// ( TEMPLATE LITERALS USANDO BACKTICKS - PERMITE EXECUTAR JAVASCRIPT DENTRO DE UMA STRING )
+console.log(`A multiplicação de 5 por 3 é ${5*3}`);
+
+// ( CONCATENAÇÃO DE STRINGS )
+console.log("Marcos," + " 39 anos," + " Casado," + " Cristão.");
